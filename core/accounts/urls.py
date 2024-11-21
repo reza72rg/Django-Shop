d7c1354693676d76ad06ed6423d9eb7924f45fda
reza72rg <reza72rg@gmail.com>
2024-11-21 19:04:03 +0330

    I create app accounts

---
+++ core/accounts/urls.py
@@ -0,0 +1,12 @@
+from urllib.parse import urlparse
+from .views import *
+from django.urls import path, include
+
+# Set the app name for namespacing
+app_name = "accounts"
+
+
+urlpatterns = [
+   #  path("", IndexViews.as_view(), name= "index")
+
+]
--- core/core/settings.py
+++ core/core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     # My apps
     "home.apps.HomeConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
