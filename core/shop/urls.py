421d39db365fb5f12998305e62b5b724f8f423a9
reza72rg <reza72rg@gmail.com>
2024-11-22 17:42:18 +0330

    I create detaile product

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -2,6 +2,7 @@ from urllib.parse import urlparse
 from .views import (
     ShopProductListView,
     ShopProductGridView,
+    ShopProductDetailsView,
 
     )
 from django.urls import path, include
@@ -13,5 +14,6 @@ app_name = "shop"
 urlpatterns = [
     path("product/list/", ShopProductListView.as_view(), name="product_list"),
     path("product/grid/",ShopProductGridView.as_view(),name="product_grid"),
+    path("product/<slug:slug>/details/", ShopProductDetailsView.as_view(), name="product_details"),
 
 ]
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,14 +1,52 @@
-from django.views.generic import TemplateView
+from tempfile import template
+
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    )
 from django.shortcuts import render
+from .models import ProductModel, ProductStatusType, ProductCategoryModel
+
 
 # Create your views here.
 
 
 
-class ShopProductListView(TemplateView):
+class ShopProductListView(ListView):
     template_name = "shop/products-list.html"
 
 
+    def get_queryset(self):
+        return  ProductModel.objects.filter(status=ProductStatusType.publish.value)
 
-class ShopProductGridView(TemplateView):
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['total_items']  = self.get_queryset().count()
+        return context
+
+
+
+class ShopProductGridView(ListView):
     template_name = "shop/products-grid.html"
+    paginate_by = 6
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size', self.paginate_by)
+
+    def get_queryset(self):
+        return  ProductModel.objects.filter(status=ProductStatusType.publish.value)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_items"] = self.get_queryset().count()
+
+        context["categories"] = ProductCategoryModel.objects.all()
+        return context
+
+
+class ShopProductDetailsView(DetailView):
+    template_name = "shop/product-detail.html"
+    queryset = ProductModel.objects.filter(
+        status=ProductStatusType.publish.value)
+
