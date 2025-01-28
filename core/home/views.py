d866049a6b541efe950422be4da9cdae2e3aa5fe
reza72rg <reza72rg@gmail.com>
2025-01-28 18:19:19 +0330

    I add cart context_processors to my context_processors

--- core/home/views.py
+++ core/home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
 # Create your views here.
 
 
 
-class IndexViews(TemplateView):
+class IndexViews(View):
     template_name = "home/index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
 
 
 class ContactView(TemplateView):
